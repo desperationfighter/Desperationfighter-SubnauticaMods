deaf0d957a8c8c3caabc2422fbcf2639c5efa5cf
desperationfighter <patrick_as_desp@yahoo.de>
2020-12-25 01:52:41 +0100

    Add project files.

---
+++ TestMod/Desp_TestMod_SN.cs
@@ -0,0 +1,35 @@
+﻿//default
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//for Assembly info
+using System.Reflection;
+//Loading Harmony for Patching
+using HarmonyLib;
+//Loading QMod as Base
+using QModManager.API.ModLoading;
+//to show a test
+using QModManager.API;
+//for Logging
+using QModManager.Utility;
+
+namespace TestMod
+{
+    [QModCore]
+    public static class Desp_TestMod_SN_Class
+    {
+        [QModPatch]
+        public static void Desp_TestMod_SN_InitializationMethod()
+        {
+            Logger.Log(Logger.Level.Debug, "Desp_TestMod_SN_Initialization");
+
+            Harmony harmony = new Harmony("Desp_TestMod_SN");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Logger.Log(Logger.Level.Info, "Desp_TestMod_SN Patched");
+
+            QModServices.Main.AddCriticalMessage("Warning instable Mod - Don´t use it !");
+        }
+    }
+}
---
+++ TestMod/Patches/uGui_SignInput_Awake_Patch.cs
@@ -0,0 +1,76 @@
+﻿//default
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//added
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestMod.Patches
+{
+    [HarmonyPatch(typeof(uGUI_SignInput))]
+    [HarmonyPatch("Awake")]
+    public static class uGui_SignInput_Awake_Patch
+    {
+		private static void Postfix(uGUI_SignInput __instance)
+        {
+			QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Desp_TestMod_SN Start Postfix");
+			if (IsOnSmallLocker(__instance))
+			{
+				QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Desp_TestMod_SN Postfix IsOnSmallLocker");
+				PatchSmallLocker(__instance);
+				QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Desp_TestMod_SN Postfix IsOnSmallLocker Patched");
+			}
+			else if (IsOnSign(__instance))
+			{
+				QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Desp_TestMod_SN Postfix IsOnSign");
+				PatchSign(__instance);
+				QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Desp_TestMod_SN Postfix IsOnSign Patched");
+			}
+		}
+
+		private static bool IsOnSmallLocker(uGUI_SignInput __instance)
+		{
+			QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Desp_TestMod_SN IsOnSmallLocker");
+			var root = __instance.gameObject.GetComponentInParent<Constructable>();
+			return root.gameObject.name.Contains("SmallLocker");
+		}
+		private static bool IsOnSign(uGUI_SignInput __instance)
+		{
+			QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Desp_TestMod_SN IsOnSmallLocker");
+			var root = __instance.gameObject.GetComponentInParent<Constructable>();
+			return root.gameObject.name.Contains("Sign");
+		}
+
+		private const float TextFieldHeight = 600;
+		//public int SmallLockerTextLimit = 60;
+		//public int SignTextLimit = 100;
+
+		private static void PatchSmallLocker(uGUI_SignInput __instance)
+		{
+			QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Desp_TestMod_SN PatchSmallLocker Methode begin");
+			__instance.inputField.lineType = InputField.LineType.MultiLineNewline;
+			__instance.inputField.characterLimit = 60;
+
+			var rt = __instance.inputField.transform as RectTransform;
+			RectTransformExtensions.SetSize(rt, rt.rect.width, TextFieldHeight);
+
+			GameObject.Destroy(__instance.inputField.textComponent.GetComponent<ContentSizeFitter>());
+			rt = __instance.inputField.textComponent.transform as RectTransform;
+			RectTransformExtensions.SetSize(rt, rt.rect.width, TextFieldHeight);
+
+			__instance.inputField.textComponent.alignment = TextAnchor.MiddleCenter;
+		}
+
+		private static void PatchSign(uGUI_SignInput __instance)
+		{
+			QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Desp_TestMod_SN PatchSign Methode begin");
+			__instance.inputField.characterLimit = 100;			
+		}
+
+	}
+}
