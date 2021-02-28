c9c7e3c3cd5edff30d19b0ff205c08d5ccd79c7d
desperationfighter <patrick_as_desp@yahoo.de>
2021-02-28 20:16:25 +0100

    messing around with prawn settings

--- MetalHands/Patches/BreakableResource_Patch.cs
+++ MetalHands/Patches/BreakableResource_Patch.cs
@@ -1,8 +1,10 @@
 ﻿using HarmonyLib;
 using MetalHands.Managment;
 using MetalHands.Items;
-
 using UnityEngine;
+//for Logging
+using QModManager.API;
+using QModManager.Utility;
 
 namespace MetalHands.Patches
 {
@@ -76,33 +78,56 @@ namespace MetalHands.Patches
                 GameObject gameObject = __instance.ChooseRandomResource();
                 if (gameObject)
                 {
-                    if( Utils.GetLocalPlayerComp().GetInMechMode() )
+                    //Exosuit exosuit = Player.main.currentMountedVehicle as Exosuit;
+                    Exosuit exosuit = Player.main.GetVehicle() as Exosuit;
+
+                    //if( Utils.GetLocalPlayerComp().GetInMechMode() )
+                    //if ( Utils.GetLocalPlayerComp().IsPiloting() )
+                    //if ( exosuit.CanPilot() && exosuit.GetPilotingMode() )
+                    if (exosuit.GetPilotingMode())
                     {
+                        QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Is Piloting");
+                        QModServices.Main.AddCriticalMessage("Is Piloting");
                         //Vehicle vehicle = Player.main.GetVehicle();
-                        Exosuit exosuit = Player.main.currentMountedVehicle as Exosuit;
-                        Vector2int size = CraftData.GetItemSize(CraftData.GetTechType(gameObject));
-
-                        Pickupable pickupable = new Pickupable( ??? gameObject);
-
-                        if (exosuit.storageContainer.container.HasRoomFor(pickupable))
-                        //if (exosuit.storageContainer.container.HasRoomFor(size.x,size.y))
+                        
+                        Pickupable pickupable = gameObject.GetComponent<Pickupable>();
+                        
+                        if( !(exosuit == null) )
                         {
-                            //exosuit.storageContainer.container.UnsafeAdd(item);
-                            exosuit.storageContainer.container.AddItem(pickupable);
-                            
+                            QModServices.Main.AddCriticalMessage("vehiocle found");
+                            QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Vehicle Found");
+
+                            if (exosuit.storageContainer.container.HasRoomFor(pickupable))
+                            //if (exosuit.storageContainer.container.HasRoomFor(size.x,size.y))
+                            {
+                                QModServices.Main.AddCriticalMessage("has room aadding to inv");
+                                QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "has room aadding to inv");
+                                InventoryItem item = new InventoryItem(pickupable);
+                                //exosuit.storageContainer.container.UnsafeAdd(item);
+                                exosuit.storageContainer.container.AddItem(pickupable);   
+                            }
+                            else
+                            {
+                                QModServices.Main.AddCriticalMessage("Has no room");
+                                QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "Has no room");
+                                __instance.SpawnResourceFromPrefab(gameObject);
+                            }
                         }
                         else
                         {
+                            QModServices.Main.AddCriticalMessage("no vehicle found");
+                            QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Debug, "no vehicle found");
                             __instance.SpawnResourceFromPrefab(gameObject);
                         }
-
                     }
-                    else if( Inventory.main.equipment.GetTechTypeInSlot("Gloves") == MetalHands.GloveMK2BlueprintTechType )
-                    {
-                        CraftData.AddToInventory(CraftData.GetTechType(gameObject));
-                    }
+                    //outcommend just for test. Works as aspected
+                    //else if( (Inventory.main.equipment.GetTechTypeInSlot("Gloves") == MetalHands.GloveMK2BlueprintTechType) && ( (Utils.GetLocalPlayerComp().IsPiloting()) == false ) )
+                    //{
+                    //    CraftData.AddToInventory(CraftData.GetTechType(gameObject));
+                    //}
                     else
                     {
+                        QModServices.Main.AddCriticalMessage("Is not piloting and no glove");
                         __instance.SpawnResourceFromPrefab(gameObject);
                     }                   
                     flag = true;
