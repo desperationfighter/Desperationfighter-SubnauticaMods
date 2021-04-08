925b83043e7c0b421fcd8d9c0cb3e64fd8758832
desperationfighter <patrick_as_desp@yahoo.de>
2021-04-08 18:56:27 +0200

    Added StorageInfo (Prefix Port)

---
+++ StorageInfo_BZ/Patches/StorageContainer_Patch.cs
@@ -0,0 +1,62 @@
+﻿using HarmonyLib;
+
+namespace StorageInfo_BZ.Patches
+{
+    [HarmonyPatch(typeof(StorageContainer))]
+    [HarmonyPatch(nameof(StorageContainer.OnHandHover))]
+    public static class StorageContainer_OnHandHover_Patch
+    {
+        public static bool Prefix(StorageContainer __instance)
+        {
+            OnHandHover_prefix(__instance);
+            return false;
+        }
+
+        public static void OnHandHover_prefix(StorageContainer __instance)
+        {
+            if (!__instance.enabled)
+            {
+                return;
+            }
+            if (__instance.disableUseability)
+            {
+                return;
+            }
+            Constructable component = __instance.gameObject.GetComponent<Constructable>();
+            if (!component || component.constructed)
+            {
+                HandReticle.main.SetText(HandReticle.TextType.Hand, __instance.hoverText, true, GameInput.Button.LeftHand);
+
+                if (__instance != null)
+                {
+                    string customInfoText = string.Empty;
+                    ItemsContainer container = __instance.container;
+
+                    if (container != null)
+                    {
+                        if (container.count <= 0)
+                        {
+                            customInfoText = "Empty";
+                        }
+                        else if (!container.HasRoomFor(1, 1))
+                        {
+                            customInfoText = "Full";
+                        }
+                        else
+                        {
+                            string Count = container.count.ToString();
+                            customInfoText = Count + " Items";
+                        }
+
+                        HandReticle.main.SetText(HandReticle.TextType.HandSubscript, customInfoText, true, GameInput.Button.None);
+                    }
+                }
+                else
+                {
+                    HandReticle.main.SetText(HandReticle.TextType.HandSubscript, __instance.IsEmpty() ? "Empty" : string.Empty, true, GameInput.Button.None);
+                }
+                HandReticle.main.SetIcon(HandReticle.IconType.Hand, 1f);
+            }
+        }
+    }
+}
---
+++ StorageInfo_BZ/StorageInfo_BZ.cs
@@ -0,0 +1,30 @@
+﻿//for Assembly info
+using System.Reflection;
+//Loading Harmony for Patching
+using HarmonyLib;
+//Loading QMod as Base
+using QModManager.API.ModLoading;
+//for Logging
+using QModManager.Utility;
+
+/* 
+ * Special Thanks goes to DingoDjango/saipop for the idea from the Base Game of Subnautica.
+ * This Version is not a direct port. It is self written but heavy inspired by the original Author.
+ */
+
+namespace StorageInfo_BZ
+{
+    public class StorageInfo_BZ
+    {
+        [QModPatch]
+        public static void StorageInfo_BZ_Initial()
+        {
+            Logger.Log(Logger.Level.Debug, "StorageInfo_BZ Initialization");
+
+            Harmony harmony = new Harmony("StorageInfo_BZ");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Logger.Log(Logger.Level.Info, "StorageInfo_BZ  Patched");
+        }
+    }
+}
