932ee351ffe151dc0ea2dd0f11628aad227166b4
desperationfighter <patrick_as_desp@yahoo.de>
2021-04-10 13:39:32 +0200

    First Nexus Release Version

--- StorageInfo_BZ/Patches/StorageContainer_Patch.cs
+++ StorageInfo_BZ/Patches/StorageContainer_Patch.cs
@@ -19,17 +19,23 @@ namespace StorageInfo_BZ.Patches
         [HarmonyTranspiler]
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
+            //Deep Logging
+            bool deeplogging = false;
+            if(!deeplogging)
+            {
+                Logger.Log(Logger.Level.Debug, "Deeploging deactivated");
+            }
+            
             Logger.Log(Logger.Level.Debug, "Start Transpiler");
 
             var getFullState = typeof(StorageContainer_OnHandHover_Patch).GetMethod("Getfullstate", BindingFlags.Public | BindingFlags.Static);
-            var getEmtpyState_methode = typeof(StorageContainer_OnHandHover_Patch).GetMethod("GetEmptyState", BindingFlags.Public | BindingFlags.Static);
             var stringEmpty = AccessTools.Field(typeof(string), "Empty");
             bool found = false;
             var Index = -1;
             var codes = new List<CodeInstruction>(instructions);
 
             //logging before
-            if (true)
+            if (deeplogging)
             {
                 Logger.Log(Logger.Level.Debug, "Deep Logging pre-transpiler:");
                 for (int k = 0; k < codes.Count; k++)
@@ -42,17 +48,6 @@ namespace StorageInfo_BZ.Patches
             Logger.Log(Logger.Level.Debug, "Start code analyses");
             for (var i = 0; i < codes.Count; i++)
             {
-                /*
-                if (codes[i].opcode == OpCodes.Ldsfld && codes[i + 2].opcode == OpCodes.Ldstr && codes[i].operand == stringEmpty)
-                {
-                    Logger.Log(Logger.Level.Debug, "Found IL Code Line for Index");
-                    Logger.Log(Logger.Level.Debug, $"Index = {Index.ToString()}");
-                    found = true;
-                    Index = i;
-                    Index2 = i + 2;
-                    break;
-                }
-                */
                 /*
                 1 IL_0048: call instance bool StorageContainer::IsEmpty()
                 2 IL_004D: brtrue.s IL_0056
@@ -91,27 +86,8 @@ namespace StorageInfo_BZ.Patches
             if (Index > -1)
             {
                 Logger.Log(Logger.Level.Debug, "Index1 > -1");
-
-                /*
-1 IL_003C: callvirt  instance void HandReticle::SetText(valuetype HandReticle/TextType, string, bool, valuetype GameInput/Button)
-2 IL_0041: ldsfld    class HandReticle HandReticle::main
-3 IL_0046: ldc.i4.1
-4 IL_0047: ldarg.0
-5 IL_0048: call      instance bool StorageContainer::IsEmpty()
-6 IL_004D: brtrue.s  IL_0056
-7 IL_004F: ldsfld    string [mscorlib]System.String::Empty
-8 IL_0054: br.s      IL_005B
-9 IL_0056: ldstr     "Empty"
-                */
-
-                //codes[Index] = new CodeInstruction(OpCodes.Call, getFullState); //override the Line 7 with a function that returns a string with the current state how much items are stored.
-                //codes[Index2] = new CodeInstruction(OpCodes.Call, getEmtpyState_methode); //override the Lin 9 with a function that returns Empty but with the available space
-
-                //codes.Insert(Index2, new CodeInstruction(OpCodes.Ldarg_0)); //inster a this reference between Line 8 and 9 to be able to acces the current Stoirage instance         
-                //codes.Insert(Index, new CodeInstruction(OpCodes.Ldarg_0)); //insert a this reference between Line 6 and 7 to be able to access the currend Storage instance
-
+                Logger.Log(Logger.Level.Info, "Transpiler injectection position found");
                 codes[Index] = new CodeInstruction(OpCodes.Call, getFullState);
-
                 codes.RemoveRange(Index + 1, 4);
             }
             else
@@ -120,7 +96,7 @@ namespace StorageInfo_BZ.Patches
             }
 
             //logging after
-            if (true)
+            if (deeplogging)
             {
                 Logger.Log(Logger.Level.Debug, "Deep Logging after-transpiler:");
                 for (int k = 0; k < codes.Count; k++)
@@ -133,19 +109,6 @@ namespace StorageInfo_BZ.Patches
             return codes.AsEnumerable();      
         }
 
-        public static int GetOriginalSize(StorageContainer _storageContainer)
-        {
-            return _storageContainer.container.sizeX * _storageContainer.container.sizeY;
-        }
-
-        public static string GetEmptyState(StorageContainer _storageContainer)
-        {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("Empty");
-            stringBuilder.AppendLine($"{GetOriginalSize(_storageContainer).ToString()} Free");
-            return stringBuilder.ToString();
-        }
-
         public static string Getfullstate(StorageContainer _storageContainer)
         {
             Logger.Log(Logger.Level.Debug, "call Getfullstate");
@@ -153,12 +116,14 @@ namespace StorageInfo_BZ.Patches
             var items = _storageContainer.container.GetItemTypes();
             int itemscount = _storageContainer.container.count;
             Logger.Log(Logger.Level.Debug, $"Itemcount = {itemscount.ToString()}");
-            int origSize = GetOriginalSize(_storageContainer);
+            int origSize = _storageContainer.container.sizeX * _storageContainer.container.sizeY;
             int usedSize = 0;
             foreach (var i in items)
             {
                 var size = TechData.GetItemSize(i);
-                usedSize += size.x * size.y;
+                int numberofsingletechtype = (_storageContainer.container.GetItems(i)).Count;
+                Logger.Log(Logger.Level.Debug, $"TEST2 - number of items = {numberofsingletechtype.ToString()}");
+                usedSize += size.x * size.y * numberofsingletechtype;
             }
             var sizeLeft = origSize - usedSize;
 
