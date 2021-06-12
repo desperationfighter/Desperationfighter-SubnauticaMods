955a033617b3caa1012508ef989c715a7f97fc69
desperationfighter <patrick_as_desp@yahoo.de>
2021-06-13 01:02:22 +0200

    Fix Config Settings

--- BetterThumper/Management/IngameConfigMenu.cs
+++ BetterThumper/Management/IngameConfigMenu.cs
@@ -8,10 +8,10 @@ namespace BetterThumper.Managment
     {
         public IngameConfigMenu() : base("config") { }
 
-        [Slider("Energy Usage", 0.000f, 0.250f, Step = 0.025f, DefaultValue = 0.125f, Tooltip = "[Default=0.075  Vanilla=0.125] Changes the Battery Drain on usage.")]
+        [Slider("Energy Usage", 0.000f, 0.250f, Step = 0.025f, DefaultValue = 0.125f, Format = "{0:F}", Tooltip = "[Default=0.075 / Vanilla=0.125] Changes the Battery Drain on usage.")]
         public float energyPerSecond = 0.075f;
 
-        [Slider("Safe Range", 30f, 330f, Step = 15f, DefaultValue = 120f, Tooltip = "[Default=120 Vanilla=60] Change the Safe Radius that the Thumper provide")]
-        public float thumperEffectRadius = 120f;
+        [Slider("Safe Range", 30f, 180f, Step = 5f, DefaultValue = 90f, Tooltip = "[Default=90 / Vanilla=60] Change the Safe Radius that the Thumper provide")]
+        public float thumperEffectRadius = 90f;
     }
 }
