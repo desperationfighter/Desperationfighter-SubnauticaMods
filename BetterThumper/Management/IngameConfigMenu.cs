d1f4132234037958e00ad21e9fcb16e9a87eec21
desperationfighter <patrick_as_desp@yahoo.de>
2021-06-17 20:44:39 +0200

    Nexus Release

--- BetterThumper/Management/IngameConfigMenu.cs
+++ BetterThumper/Management/IngameConfigMenu.cs
@@ -11,7 +11,7 @@ namespace BetterThumper.Managment
         [Slider("Energy Usage", 0.000f, 0.250f, Step = 0.025f, DefaultValue = 0.125f, Format = "{0:F}", Tooltip = "[Default=0.075 / Vanilla=0.125] Changes the Battery Drain on usage.")]
         public float energyPerSecond = 0.075f;
 
-        [Slider("Safe Range", 30f, 180f, Step = 5f, DefaultValue = 90f, Tooltip = "[Default=90 / Vanilla=60] Change the Safe Radius that the Thumper provide")]
+        [Slider("Safe Range", 30f, 180f, Step = 5f, DefaultValue = 90f, Tooltip = "[Default=90 / Vanilla=~60] Change the Safe Radius that the Thumper provide")]
         public float thumperEffectRadius = 90f;
     }
 }
