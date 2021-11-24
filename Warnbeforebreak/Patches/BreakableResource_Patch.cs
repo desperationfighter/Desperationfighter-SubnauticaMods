35abafe14117ea63fa1d87257e29446107e6bac1
desperationfighter <patrick_as_desp@yahoo.de>
2021-11-24 19:50:06 +0100

    Initial

---
+++ Warnbeforebreak/Patches/BreakableResource_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+//for Logging
+using QModManager.Utility;
+
+namespace Warnbeforebreak.Patches
+{
+    [HarmonyPatch(typeof(BreakableResource))]
+    [HarmonyPatch(nameof(BreakableResource.OnHandHover))]
+    public static class BreakableResource_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix(BreakableResource __instance)
+        {
+            Logger.Log(Logger.Level.Debug, "Breakresource - OnHandover - loaded");
+            if ( ! (Player.main.HasInventoryRoom(1, 1) ) )
+            {
+                Logger.Log(Logger.Level.Debug, "Has NO Room");
+                HandReticle.main.SetInteractText(__instance.breakText, "Inventory Full !");
+                HandReticle.main.SetIcon(HandReticle.IconType.HandDeny, 1f);
+            }
+        }
+    }
+}
---
+++ Warnbeforebreak/Warnbeforebreak.cs
@@ -0,0 +1,24 @@
+﻿//for Assembly info
+using System.Reflection;
+//Loading Harmony for Patching
+using HarmonyLib;
+//Loading QMod as Base
+using QModManager.API.ModLoading;
+//for Logging
+using QModManager.Utility;
+
+namespace Warnbeforebreak
+{
+    [QModCore]
+    public static class Warnbeforebreak
+    {
+        [QModPatch]
+        public static void wrnbeforebreak_InitializationMethod()
+        {
+            Logger.Log(Logger.Level.Debug, "Warnbeforebreak Initialization");
+            Harmony harmony = new Harmony("Warnbeforebreak");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Logger.Log(Logger.Level.Info, "Warnbeforebreak Patched");
+        }
+    }
+}
