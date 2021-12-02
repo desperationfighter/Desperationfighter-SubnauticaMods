6bf4c8573633ea6201bc4efd916748e600f826fb
desperationfighter <patrick_as_desp@yahoo.de>
2021-12-02 22:30:32 +0100

    Storage Info SN Initial - Will be not released

---
+++ StorageInfo_SN/StorageInfo_SN.cs
@@ -0,0 +1,26 @@
+﻿//for Assembly info
+using System.Reflection;
+//Loading Harmony for Patching
+using HarmonyLib;
+//Loading QMod as Base
+using QModManager.API.ModLoading;
+//for Logging
+using QModManager.Utility;
+
+namespace StorageInfo_SN
+{
+    [QModCore]
+    public static class StorageInfo_SN
+    {
+        [QModPatch]
+        public static void StorageInfo_SN_Initial()
+        {
+            Logger.Log(Logger.Level.Debug, "StorageInfo_SN Initialization");
+
+            Harmony harmony = new Harmony("StorageInfo_SN");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Logger.Log(Logger.Level.Info, "StorageInfo_SN  Patched");
+        }
+    }
+}
