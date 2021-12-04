952f6f54f01ff5a6cfbaeb8c3a165922a71551ad
desperationfighter <patrick_as_desp@yahoo.de>
2021-12-04 15:55:51 +0100

    Added Inventory Count when not full

--- Warnbeforebreak/Patches/BreakableResource_Patch.cs
+++ Warnbeforebreak/Patches/BreakableResource_Patch.cs
@@ -1,6 +1,4 @@
 ﻿using HarmonyLib;
-//for Logging
-using QModManager.Utility;
 
 namespace Warnbeforebreak.Patches
 {
@@ -11,13 +9,38 @@ namespace Warnbeforebreak.Patches
         [HarmonyPostfix]
         private static void Postfix(BreakableResource __instance)
         {
-            Logger.Log(Logger.Level.Debug, "Breakresource - OnHandover - loaded");
-            if ( ! (Player.main.HasInventoryRoom(1, 1) ) )
+            HandReticle.main.SetInteractText(__instance.breakText, GetInventoryleftspace());
+            if (!(Player.main.HasInventoryRoom(1, 1)))
             {
-                Logger.Log(Logger.Level.Debug, "Has NO Room");
-                HandReticle.main.SetInteractText(__instance.breakText, "Inventory Full !");
                 HandReticle.main.SetIcon(HandReticle.IconType.HandDeny, 1f);
             }
+        }
+
+        public static string GetInventoryleftspace()
+        {
+            if (!(Player.main.HasInventoryRoom(1, 1)))
+            {
+                return "Inventory Full !";
+            }
+            else
+            {
+                Inventory Inv = Player.main.GetComponent<Inventory>();
+                int Size_x = Inv.container.sizeX;
+                int Size_y = Inv.container.sizeY;
+                int Size_xy = Size_y * Size_x;
+                var Items = Inv.container.GetItemTypes();
+                int usedSize = 0;
+                foreach (var i in Items)
+                {
+                    var size = CraftData.GetItemSize(i);
+                    int numberofsingletechtype = (Inv.container.GetItems(i)).Count;
+                    usedSize += size.x * size.y * numberofsingletechtype;
+                }
+                var sizeLeft = Size_xy - usedSize;
+                string returnstring = sizeLeft.ToString() + " of " + Size_xy + " free";
+                return returnstring;
+            }
+
         }
     }
 }
